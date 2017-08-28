478271ed5fac77771d29c47a2bc82a346d924edc
Fabio Araujo <fabio_araujo11@hotmail.com>
2017-08-27 22:58:26 -0300

    Adicionado bootstrap e feito parte do Home. ps.: Refeito com um nome diferente em um local diferente.

---
+++ Juros/src/java/br/com/juros/web/HomeServlet.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.juros.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+@WebServlet(name = "homeServlet", urlPatterns = {"/home.html"})
+public class HomeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html >");
+            out.println("<head>");
+            out.println("<title>PROJETO 1</title>");            
+            out.println("<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">");            
+            out.println("</head>");
+            out.println("<body style='text-align: center'>");
+            out.println("<h1>Projeto 1 - Programação Orientada a Objeto</h1>");
+            out.println("<hr />");
+            out.println("<ul class=\"nav nav-tabs nav-justified\">");
+            out.println("<a href=\"jurossimples.html\"><button type=\"button\" class=\"btn btn-default navbar-btn\">Juros Simples</button></a>");
+            out.println("<a href=\"juroscomposto.html\"><button type=\"button\" class=\"btn btn-default navbar-btn\">Juros Composto</button></a>");
+            out.println("</ul>");
+            out.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>");
+            out.println("<script src=\"js/bootstrap.min.js\"></script>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
